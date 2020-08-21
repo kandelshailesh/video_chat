5de50281d5c77cf8aeb363ea7f316e330c857dc1
Shailesh <shaileshkandel123@gmail.com>
2020-08-21 08:59:44 +0545

    Updates

---
+++ precache-manifest.430cfbe302dfe76f8bc0d7433d94bd28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2255715076f6bf366ce8a3d786798e49",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bb2bdf7a2aa566bfcc99",
+    "url": "/static/css/main.87c9b992.chunk.css"
+  },
+  {
+    "revision": "bb5ae0e80aa96df17d4e",
+    "url": "/static/js/2.282ec8a6.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.282ec8a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb2bdf7a2aa566bfcc99",
+    "url": "/static/js/main.3555ee31.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
