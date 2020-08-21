6c18ac47db010e79421aec4085e305bcb1956ef7
Shailesh <shaileshkandel123@gmail.com>
2020-08-21 09:18:04 +0545

    Updates

---
+++ precache-manifest.1804bbd08911fc66215576e5694d221f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2fa13008b7b235c81600fc8e560c8ef",
+    "url": "/video_chat/index.html"
+  },
+  {
+    "revision": "03ddca89415ee05c3ee3",
+    "url": "/video_chat/static/css/main.ca348e32.chunk.css"
+  },
+  {
+    "revision": "bda10a1c7859f154d14d",
+    "url": "/video_chat/static/js/2.b6679903.chunk.js"
+  },
+  {
+    "revision": "78b287351bb08f453fa0918ea10ac39c",
+    "url": "/video_chat/static/js/2.b6679903.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03ddca89415ee05c3ee3",
+    "url": "/video_chat/static/js/main.1c983c7a.chunk.js"
+  },
+  {
+    "revision": "2cca9ce0f12946d2fe63",
+    "url": "/video_chat/static/js/runtime-main.4cfbadee.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.430cfbe302dfe76f8bc0d7433d94bd28.js"
+  "/video_chat/precache-manifest.1804bbd08911fc66215576e5694d221f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/video_chat/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
